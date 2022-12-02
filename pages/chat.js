2db3224edbd697128e22b5417e5d9a9bb2fd1790
Jst Vex <heinhtetlulu14@gmail.com>
2022-12-02 22:21:01 +0630

    added chat page

---
+++ pages/chat.js
@@ -0,0 +1,12 @@
+import Sidebar from '../components/Sidebar';
+
+const Chat = () => {
+    return (
+        <div>
+            <Sidebar />
+            <h1>this is the chat page</h1>
+        </div>
+    );
+}
+
+export default Chat;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Sidebar from '../components/Sidebar'
 import styles from '../styles/Home.module.css'
+import Chat from './chat'
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Sidebar />
+      <Chat />
 
     </div>
   )
